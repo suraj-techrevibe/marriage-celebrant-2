c8b6c56d19febe462a1f403a50a89d9d65c637bf
unknown <shrestha.suraj.2013@gmail.com>
2026-03-01 21:02:05 +1100

    Update project

--- src/main.tsx
+++ src/main.tsx
@@ -3,15 +3,14 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes/AppRouter";
 import "./styles/index.css"
-import { EditProvider } from "./editable/context/EditContext";
 import "./styles/animation.css"
 import React from "react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <EditProvider>
+  
       
     <RouterProvider router={router} />
-    </EditProvider>
+   
   </React.StrictMode>
 );
